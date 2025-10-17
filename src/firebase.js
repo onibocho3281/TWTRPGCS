e876d66675e64a05eef0aaa869dc48726d45a612
onibocho3281 <onibocho3281@gmail.com>
2025-10-17 20:26:20 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,19 @@
+// firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-const firebaseConfig = { /* your config */ };
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  appId: process.env.REACT_APP_FIREBASE_APPID,
+};
+
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
-// Required Google API scopes
+// Add scopes required for Drive/Sheets
 provider.addScope("https://www.googleapis.com/auth/drive.file");
 provider.addScope("https://www.googleapis.com/auth/spreadsheets");
 provider.addScope("https://www.googleapis.com/auth/drive.metadata.readonly");
